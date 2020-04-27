b66cd4c859dae194cfd0bf92eadc993be1cfe699
Rashi <agata.rachanska.programista@gmail.com>
2020-04-27 15:23:09 +0200

    Test class UsersMenager

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
+#include "Markup.h"
+#include "UsersMenager.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    UsersMenager usersMenager;
+    usersMenager.rejestracjaUzytkownika();
+    usersMenager.rejestracjaUzytkownika();
+    usersMenager.wyswietlWszystkichUzytkownikow();
+    //usersMenager.logowanieUzytkownika();
+    //usersMenager.wylogowanieZalogowanegoUzytkownika();
+    //usersMenager.zmianaHaslaZalogowanegoUzytkownika();
+    //usersMenager.wyswietlWszystkichUzytkownikow();
+
     return 0;
 }
